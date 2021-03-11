74168c0f454806d1c2ee0bd1b7a7dcb4f92a433f
Kliu709 <kliu709@uchicago.edu>
2021-03-11 14:45:12 +0000

    users generated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_11_144346) do
+ActiveRecord::Schema.define(version: 2021_03_11_144444) do
 
   create_table "followers", force: :cascade do |t|
     t.integer "sender_id"
@@ -25,6 +25,14 @@ ActiveRecord::Schema.define(version: 2021_03_11_144346) do
     t.string "day_of_week"
     t.integer "user_id"
     t.string "effort"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
+    t.string "username"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
